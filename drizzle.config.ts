fce73ae9cef4b01d9c404424c40430e7a900480c
DouglasMoraesDev <doougmooraes2024@gmail.com>
2025-04-23 21:02:01 -0300

    Atualizar o drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,13 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const connectionString = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 export default defineConfig({
   schema: './src/database/schema',
   out: './drizzle',
   driver: 'mysql2',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString,
   },
 });
