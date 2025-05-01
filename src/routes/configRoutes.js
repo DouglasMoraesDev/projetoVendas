51ace4f6a161f1729babd5f589ad06bc92143a55
DouglasMoraesDev <doougmooraes2024@gmail.com>
2025-05-01 20:24:33 -0300

    commit testet 5

--- src/routes/configRoutes.js
+++ src/routes/configRoutes.js
@@ -1,15 +1,11 @@
 // src/routes/configRoutes.js
 import { Router } from "express";
 import { ConfigController } from "../controllers/ConfigController.js";
-// Se você fez `export function authMiddleware` no auth.js:
-//    ↑ nome exato entre chaves
-import { authMiddleware } from "../middlewares/authMiddleware.js";
-// Se você fez `export default authMiddleware` no auth.js, use:
-// import authMiddleware from "../middlewares/auth.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.get("/backup", authMiddleware, ConfigController.backup);
-router.get("/auditoria", authMiddleware, ConfigController.auditoria);
+router.get("/backup", authenticate, ConfigController.backup);
+router.get("/auditoria", authenticate, ConfigController.auditoria);
 
 export default router;
